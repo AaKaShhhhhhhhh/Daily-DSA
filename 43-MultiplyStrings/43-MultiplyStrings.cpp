f12a6440e3c9afaced9328f179b115a2005d0e5f
Aakash Kamal <159654922+AaKaShhhhhhhhh@users.noreply.github.com>
2025-07-30 10:47:53 +0530

    Create 43-MultiplyStrings

---
+++ 43-MultiplyStrings/43-MultiplyStrings.cpp
@@ -0,0 +1,30 @@
+// Last updated: 7/30/2025, 10:47:54 AM
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        
+         int n = num1.size();
+        int m = num2.size();
+        vector<int> result(m+n, 0);
+
+       
+        for(int i = n-1 ; i>=0 ; i--){
+            for(int j = m-1 ; j>=0 ; j--){
+
+                int mul = (num1[i] - '0')*(num2[j] - '0');
+                int sum = mul + result[i+j+1];
+
+                result[i+j+1] = sum%10;
+                result[i+j] += sum/10;
+
+            }
+        }
+           string ans = "" ;
+        for(int num : result){
+            if(!(ans.empty() && num == 0)){
+                ans += to_string(num);
+            }
+        }
+        return ans.empty() ? "0" : ans;
+    }
+};
