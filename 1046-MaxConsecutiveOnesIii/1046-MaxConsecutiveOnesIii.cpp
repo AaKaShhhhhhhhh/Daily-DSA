d961d9ee3998f9e5058a3e35912b9af70ccf0ce2
Aakash Kamal <159654922+AaKaShhhhhhhhh@users.noreply.github.com>
2025-07-30 10:46:56 +0530

    Create 1046-MaxConsecutiveOnesIii

---
+++ 1046-MaxConsecutiveOnesIii/1046-MaxConsecutiveOnesIii.cpp
@@ -0,0 +1,25 @@
+// Last updated: 7/30/2025, 10:46:57 AM
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int n = nums.size();
+        int left = 0 ;
+        int right = 0; int cnt =0;int maxlen = 0;
+        while(right<n){
+            if(nums[right] == 0){
+                cnt++;
+            }
+            while(cnt>k){
+                if(nums[left]==0){
+                    cnt--;
+
+            }
+            left++;
+            }
+            maxlen = max(maxlen, right-left+1);
+            right++;
+            
+        }
+        return maxlen;
+    }
+};
