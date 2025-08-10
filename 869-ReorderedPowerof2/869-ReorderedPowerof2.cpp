9d3806ed17693cec9f8b7f71307bfea531ab04b9
Aakash Kamal <159654922+AaKaShhhhhhhhh@users.noreply.github.com>
2025-08-10 12:28:14 +0530

    Create 869-ReorderedPowerof2

---
+++ 869-ReorderedPowerof2/869-ReorderedPowerof2.cpp
@@ -0,0 +1,29 @@
+// Last updated: 8/10/2025, 12:28:14 PM
+class Solution {
+public:
+    bool reorderedPowerOf2(int n) {
+        // unordered_map<int,int> freq;
+
+        // int cnt = 0;
+        // while(n>0){
+        //     int digits = n%10;
+        //     freq[digits]++;
+        //     cnt++;
+        //     n/=10;
+        // }
+        // for(int i : freq){
+
+        // }
+        string str = to_string(n);
+        sort(str.begin() , str.end());
+        for(int i = 0 ; i<30 ; i++){
+            int pow = 1<<i;
+
+            string s = to_string(pow);
+            sort(s.begin() , s.end());
+            if(s == str)return true;
+        }
+        
+        return false;
+    } 
+};
