ac327982e4c42bfbeb5978b3b2762abe50bbad14
Aakash Kamal <159654922+AaKaShhhhhhhhh@users.noreply.github.com>
2025-08-19 19:53:49 +0530

    Create 2348-NumberofZero-FilledSubarrays

---
+++ 2348-NumberofZero-FilledSubarrays/2348-NumberofZero-FilledSubarrays.cpp
@@ -0,0 +1,18 @@
+// Last updated: 8/19/2025, 7:53:49 PM
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        
+        long long ans=0 ;
+        long long cnt = 0;
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(nums[i] == 0){
+                cnt++;
+                ans+=cnt;
+            }else{
+                cnt = 0;
+            }
+        }
+        return ans;
+    }
+};
