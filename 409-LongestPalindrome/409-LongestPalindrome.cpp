31d61b0183bd79889258599eb614a201b5eecb24
Aakash Kamal <159654922+AaKaShhhhhhhhh@users.noreply.github.com>
2025-08-21 18:51:46 +0530

    Create 409-LongestPalindrome

---
+++ 409-LongestPalindrome/409-LongestPalindrome.cpp
@@ -0,0 +1,25 @@
+// Last updated: 8/21/2025, 6:51:47 PM
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_map<char,int> map;
+        int isodd = false;
+        int ans = 0;
+        for(char c : s){
+            map[c]++;
+        }
+      
+        for(auto &c : map){
+             if(c.second % 2 == 0){
+                ans += c.second;
+             }else{
+                ans+= c.second - 1;
+                isodd = true;
+             }
+             
+        }
+        if(isodd)ans+=1;
+        return ans;
+        
+    }
+};
