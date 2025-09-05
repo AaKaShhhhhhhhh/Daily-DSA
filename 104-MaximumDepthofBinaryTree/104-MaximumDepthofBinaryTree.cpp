35b73b31d06a4b70e8e305c941f4f706b420abbe
Aakash Kamal <159654922+AaKaShhhhhhhhh@users.noreply.github.com>
2025-09-05 20:03:31 +0530

    Create 104-MaximumDepthofBinaryTree

---
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.cpp
@@ -0,0 +1,27 @@
+// Last updated: 9/5/2025, 8:03:30 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == nullptr){
+            return 0;
+        }
+        int leftdep = maxDepth(root->left);
+        int rightdep = maxDepth(root->right);
+        return 1+max(leftdep , rightdep);
+
+
+        
+        
+    }
+};
