a6623632cba35b02bb1cee42010f3af32d47def5
Aakash Kamal <159654922+AaKaShhhhhhhhh@users.noreply.github.com>
2025-10-17 23:27:16 +0530

    File updated at 10/17/2025, 11:27:16 PM

--- 67-AddBinary/67-AddBinary.cpp
+++ 67-AddBinary/67-AddBinary.cpp
@@ -1,4 +1,4 @@
-// Last updated: 10/17/2025, 11:25:37 PM
+// Last updated: 10/17/2025, 11:27:16 PM
 class Solution {
 public:
     string addBinary(string a, string b) {
@@ -10,14 +10,14 @@ public:
         while (i >= 0 || j >= 0 || carry) {
             int sum = carry;
 
-            if (i >= 0) sum += a[i--] - '0';  // convert '0' or '1' to int
+            if (i >= 0) sum += a[i--] - '0';  
             if (j >= 0) sum += b[j--] - '0';
 
-            result += (sum % 2) + '0';        // current bit
-            carry = sum / 2;                  // carry for next bit
+            result += (sum % 2) + '0';       
+            carry = sum / 2;                  
         }
 
-        reverse(result.begin(), result.end()); // reverse the string
+        reverse(result.begin(), result.end()); 
         return result;
     }
 };
