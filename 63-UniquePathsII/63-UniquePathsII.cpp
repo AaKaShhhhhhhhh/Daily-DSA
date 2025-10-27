38e412b66a1725a286ba5cc227bd124ca20daade
Aakash Kamal <159654922+AaKaShhhhhhhhh@users.noreply.github.com>
2025-10-27 22:49:33 +0530

    Create 63-UniquePathsII

---
+++ 63-UniquePathsII/63-UniquePathsII.cpp
@@ -0,0 +1,24 @@
+// Last updated: 10/27/2025, 10:49:33 PM
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+         int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+         if (obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1)
+            return 0;
+        vector<vector<int>> dp(m , vector<int>(n,-1));
+        return helper(m-1 , n-1 , obstacleGrid, dp);
+    }
+    int helper(int i , int j , vector<vector<int>>& obstacleGrid,vector<vector<int>>& dp){
+                
+         
+        if (i < 0 || j < 0) return 0;  
+        if (obstacleGrid[i][j] == 1) return 0;  
+        if (i == 0 && j == 0) return 1;
+        if(dp[i][j] != -1)return dp[i][j];
+        int down = helper(i-1,j,obstacleGrid,dp );
+        int right = helper(i , j-1, obstacleGrid,dp);
+
+        return dp[i][j] = down + right;
+    }
+};
