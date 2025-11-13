2c6e9a9ff2e3ba0e5cdf33acb580744fe471ef51
Aakash Kamal <159654922+AaKaShhhhhhhhh@users.noreply.github.com>
2025-11-13 22:01:44 +0530

    Create 3228-MaximumNumberofOperationstoMoveOnestotheEnd

---
+++ 3228-MaximumNumberofOperationstoMoveOnestotheEnd/3228-MaximumNumberofOperationstoMoveOnestotheEnd.cpp
@@ -0,0 +1,14 @@
+// Last updated: 11/13/2025, 10:01:44 PM
+class Solution {
+public:
+    int maxOperations(string s) {
+        int ones = 0, ans = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '1') ones++;
+            else if (i > 0 && s[i - 1] == '1') {
+                ans+=ones;
+            }
+        }
+        return ans;
+    }
+};
