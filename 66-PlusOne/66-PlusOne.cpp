ea20d53eafcbb834e073328ef7981b3158121ee8
Aakash Kamal <159654922+AaKaShhhhhhhhh@users.noreply.github.com>
2026-01-01 19:29:12 +0530

    File updated at 1/1/2026, 7:29:11 PM

--- 66-PlusOne/66-PlusOne.cpp
+++ 66-PlusOne/66-PlusOne.cpp
@@ -1,21 +1,21 @@
-// Last updated: 7/30/2025, 10:47:47 AM
-class Solution {
-public:
-    vector<int> plusOne(vector<int>& digits) {
-        int n = digits.size();
-        vector<int> arr;
-        int carry = 1;
-        for(int i=n-1 ; i>=0 ; i--){
-            int sum = digits[i]+carry;
-
-            digits[i] = sum % 10;
-            carry = sum/10;
-
-        }
-        if(carry){
-            digits.insert(digits.begin() , carry);
-           
-        }
-         return digits;
-    }
-};
+// Last updated: 1/1/2026, 7:29:11 PM
+1class Solution {
+2public:
+3    vector<int> plusOne(vector<int>& digits) {
+4        int n = digits.size();
+5        vector<int> arr;
+6        int carry = 1;
+7        for(int i=n-1 ; i>=0 ; i--){
+8            int sum = digits[i]+carry;
+9
+10            digits[i] = sum % 10;
+11            carry = sum/10;
+12
+13        }
+14        if(carry){
+15            digits.insert(digits.begin() , carry);
+16           
+17        }
+18         return digits;
+19    }
+20};
