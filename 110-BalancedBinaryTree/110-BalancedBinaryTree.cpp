06bbb724d2388cb810e6a12ccda688e3f8a9cd52
Aakash Kamal <159654922+AaKaShhhhhhhhh@users.noreply.github.com>
2026-02-08 18:23:05 +0530

    File updated at 2/8/2026, 6:23:07 PM

--- 110-BalancedBinaryTree/110-BalancedBinaryTree.cpp
+++ 110-BalancedBinaryTree/110-BalancedBinaryTree.cpp
@@ -1,33 +1,33 @@
-// Last updated: 9/9/2025, 5:43:52 PM
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    int height(TreeNode* root){
-        if(!root)return 0;
-
-        int left = height(root->left);
-        if(left == -1)return -1;
-
-        
-        int right = height(root->right);
-        if(right == -1)return -1;
-
-        if(abs(left - right) > 1)return -1;
-
-        return max(left,right)+1;
-    }
-    bool isBalanced(TreeNode* root) {
-        return height(root) != -1;
-        
-    }
-};
+// Last updated: 2/8/2026, 6:23:07 PM
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14    int height(TreeNode* root){
+15        if(!root)return 0;
+16
+17        int left = height(root->left);
+18        if(left == -1)return -1;
+19
+20        
+21        int right = height(root->right);
+22        if(right == -1)return -1;
+23
+24        if(abs(left - right) > 1)return -1;
+25
+26        return max(left,right)+1;
+27    }
+28    bool isBalanced(TreeNode* root) {
+29        return height(root) != -1;
+30        
+31    }
+32};
