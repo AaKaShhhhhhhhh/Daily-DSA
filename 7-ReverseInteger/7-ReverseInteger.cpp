08cd34147bf224d17236042c49219522c5edf749
Aakash Kamal <159654922+AaKaShhhhhhhhh@users.noreply.github.com>
2026-03-05 22:13:11 +0530

    File updated at 3/5/2026, 10:13:12 PM

--- 7-ReverseInteger/7-ReverseInteger.cpp
+++ 7-ReverseInteger/7-ReverseInteger.cpp
@@ -1,17 +1,17 @@
-// Last updated: 5/25/2025, 12:02:19 AM
-class Solution {
-public:
-    int reverse(int x) {
-        int count=0 ;
-        while(x!=0){
-            int j = x % 10;
-             x=x/10;
-            if(count<INT_MIN/10 || count>INT_MAX/10){
-                return 0;
-            }
-            count = count*10+j;
-           
-        }
-        return count;
-    }
-};
+// Last updated: 3/5/2026, 10:13:12 PM
+1class Solution {
+2public:
+3    int reverse(int x) {
+4        int count=0 ;
+5        while(x!=0){
+6            int j = x % 10;
+7             x=x/10;
+8            if(count<INT_MIN/10 || count>INT_MAX/10){
+9                return 0;
+10            }
+11            count = count*10+j;
+12           
+13        }
+14        return count;
+15    }
+16};
