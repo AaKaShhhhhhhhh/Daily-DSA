d2adad46a6fece942addac6371b55d64809cdb1d
Aakash Kamal <159654922+AaKaShhhhhhhhh@users.noreply.github.com>
2026-05-05 21:10:18 +0530

    File updated at 5/5/2026, 9:10:18 PM

--- 61-RotateList/61-RotateList.cpp
+++ 61-RotateList/61-RotateList.cpp
@@ -1,35 +1,35 @@
-// Last updated: 6/25/2025, 10:27:48 AM
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    ListNode* rotateRight(ListNode* head, int k) {
-        if(!head || !head->next || k==0)return head;
-        int count = 1;
-        ListNode* curr = head;
-        while(curr->next!=NULL){
-            count++;
-            curr = curr->next;
-            
-        }
-        k = k%count;
-        curr->next = head;
-        int breakpoint = count-k;
-        ListNode* newtail = head;
-        for(int i=1; i<breakpoint ; i++){
-             newtail = newtail->next;
-        }
-        
-        ListNode* newhead = newtail->next;
-        newtail->next = NULL;
-        return newhead;
-    }
-};
+// Last updated: 5/5/2026, 9:10:18 PM
+1/**
+2 * Definition for singly-linked list.
+3 * struct ListNode {
+4 *     int val;
+5 *     ListNode *next;
+6 *     ListNode() : val(0), next(nullptr) {}
+7 *     ListNode(int x) : val(x), next(nullptr) {}
+8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+9 * };
+10 */
+11class Solution {
+12public:
+13    ListNode* rotateRight(ListNode* head, int k) {
+14        if(!head || !head->next || k==0)return head;
+15        int count = 1;
+16        ListNode* curr = head;
+17        while(curr->next!=NULL){
+18            count++;
+19            curr = curr->next;
+20            
+21        }
+22        k = k%count;
+23        curr->next = head;
+24        int breakpoint = count-k;
+25        ListNode* newtail = head;
+26        for(int i=1; i<breakpoint ; i++){
+27             newtail = newtail->next;
+28        }
+29        
+30        ListNode* newhead = newtail->next;
+31        newtail->next = NULL;
+32        return newhead;
+33    }
+34};
